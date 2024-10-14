e7d5ca0103a1125791a633456b7d8b07c638836e
taciturn2021 <142534049+taciturn2021@users.noreply.github.com>
2024-10-14 14:58:10 +0500

    Add walk animations

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -7,9 +7,11 @@ public class Entity {
     public int speed;
 
     public BufferedImage idleup1, idleup2, idledown1, idledown2, idleleft1, idleleft2, idleright1, idleright2; // Entity Images for standing still in each direction
+    public BufferedImage up1,up2,up3,up4,down1,down2,down3,down4,left1,left2,left3,left4,right1,right2,right3,right4; // Entity Images for moving in each direction
     public String direction; // Direction the entity is facing
     public boolean isMoving; // Boolean to check if the entity is moving
     public int idleSpriteCounter = 0; // Counter to switch between idle sprites
     public int idleSpriteNum = 1; // Number to determine which idle sprite to display
-
+    public int walkSpriteCounter = 0; // Counter to switch between walk sprites
+    public int walkSpriteNum = 1; // Number to determine which walk sprite to display
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -25,6 +25,7 @@ public class Player extends Entity{
         direction = "down";
         isMoving = false;
     }
+    // Load the player images
     public void getPlayerImage(){
         try{
             idleup1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idle/idleup1.png"));
@@ -35,6 +36,22 @@ public class Player extends Entity{
             idleleft2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idle/idleleft2.png"));
             idleright1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idle/idleright1.png"));
             idleright2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idle/idleright2.png"));
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkup-1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkup-2.png"));
+            up3 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkup-3.png"));
+            up4 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkup-4.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkdown-1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkdown-2.png"));
+            down3 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkdown-3.png"));
+            down4 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkdown-4.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkleft-1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkleft-2.png"));
+            left3 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkleft-3.png"));
+            left4 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkleft-4.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkright-1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkright-2.png"));
+            right3 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkright-3.png"));
+            right4 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/walk/walkright-4.png"));
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -85,9 +102,64 @@ public class Player extends Entity{
         return image;
     }
     // Set the walking image based on the direction
-//    public BufferedImage walking(){
-//        BufferedImage image = null;
-//    }
+    public BufferedImage walking(){
+        BufferedImage image = null;
+        switch (direction){
+            case "up":
+                if(walkSpriteNum == 1){
+                    image = up1;
+                }else if(walkSpriteNum == 2) {
+                    image = up2;
+                }
+                else if(walkSpriteNum == 3) {
+                    image = up3;
+                }
+                else if(walkSpriteNum == 4) {
+                    image = up4;
+                }
+                break;
+            case "down":
+                if(walkSpriteNum == 1){
+                    image = down1;
+                }else if(walkSpriteNum == 2) {
+                    image = down2;
+                }
+                else if(walkSpriteNum == 3) {
+                    image = down3;
+                }
+                else if(walkSpriteNum == 4) {
+                    image = down4;
+                }
+                break;
+            case "left":
+                if(walkSpriteNum == 1){
+                    image = left1;
+                }else if(walkSpriteNum == 2) {
+                    image = left2;
+                }
+                else if(walkSpriteNum == 3) {
+                    image = left3;
+                }
+                else if(walkSpriteNum == 4) {
+                    image = left4;
+                }
+                break;
+            case "right":
+                if(walkSpriteNum == 1){
+                    image = right1;
+                }else if(walkSpriteNum == 2) {
+                    image = right2;
+                }
+                else if(walkSpriteNum == 3) {
+                    image = right3;
+                }
+                else if(walkSpriteNum == 4) {
+                    image = right4;
+                }
+                break;
+        }
+        return image;
+    }
 
     public void update(){
         setMovement(); // Check if the player is moving
@@ -109,6 +181,22 @@ public class Player extends Entity{
                 if (x + speed >= gp.getScreenWidth() - gp.getTileSize()) return; // Check if the player is at the right of the screen
                 x += speed; // Move the player right
             }
+            walkSpriteCounter++; // Increment the spriteCounter
+            if (walkSpriteCounter >= 10){ // Check if the spriteCounter is greater than 10
+                if(walkSpriteNum == 1){
+                    walkSpriteNum = 2;
+                }
+                else if(walkSpriteNum == 2){
+                    walkSpriteNum = 3;
+                }
+                else if(walkSpriteNum == 3){
+                    walkSpriteNum = 4;
+                }
+                else if(walkSpriteNum == 4){
+                    walkSpriteNum = 1;
+                }
+                walkSpriteCounter = 0; // Reset the spriteCounter
+            }
         }
 
         idleSpriteCounter++; // Increment the spriteCounter
@@ -126,7 +214,7 @@ public class Player extends Entity{
     public void draw(Graphics2D g2) {
         BufferedImage image = null; // Get the idle image based on the direction
         if(!isMoving) image = idle(); // Get the idle image based on the direction
-//        if (isMoving) image = walking(); // Get the walking image based on the direction
+        if (isMoving) image = walking(); // Get the walking image based on the direction
         g2.drawImage(image, x, y, gp.getTileSize(), gp.getTileSize(), null); // Draw the player
     }
 
